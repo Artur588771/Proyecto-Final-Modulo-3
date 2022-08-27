209b4fd1529320c062e3cdfed00d50a4dae72ecf
Arturo Renteria <arthur_metal88@hotmail.com>
2022-08-27 18:50:39 -0500

    filtros completo

--- js/filtros.js
+++ js/filtros.js
@@ -15,6 +15,27 @@ let buscarRol = (rolCampeon) => {
   mostrar(filtro);
 } 
 
+let buscarRegion = (regionCampeon) => {
+  let filtro = campeones.filter((campeon) =>{
+    return campeon.region==regionCampeon;    
+  })
+  mostrar(filtro);
+}
+
+let buscarDificultad = (difiCampeon) => {
+  let filtro = campeones.filter((campeon) =>{
+    return campeon.dificultad==difiCampeon;    
+  })
+  mostrar(filtro);
+}
+
+let buscarPoder = (poderCampeon) => {
+  let filtro = campeones.filter((campeon) =>{
+    return campeon.poder==poderCampeon;    
+  })
+  mostrar(filtro);
+}
+
   //document.querySelector("#buscarForm").addEventListener("submit", buscar);
   document.querySelector("#buscarForm button").addEventListener("click", buscar);
   document.querySelector("#inputBuscar").addEventListener("keyup", buscar);
@@ -23,5 +44,26 @@ const catRol =  document.querySelectorAll(".rol");
 catRol.forEach(rol => {
   rol.addEventListener("click", function () {
     buscarRol(rol.textContent.toUpperCase());
+  });
+})
+
+const catRegion =  document.querySelectorAll(".region");
+catRegion.forEach(region => {
+  region.addEventListener("click", function () {
+    buscarRegion(region.textContent.toUpperCase());
+  });
+})
+
+const catDificultad =  document.querySelectorAll(".difi");
+catDificultad.forEach(difi => {
+  difi.addEventListener("click", function () {
+    buscarDificultad(difi.textContent.toUpperCase());
+  });
+})
+
+const catPoder =  document.querySelectorAll(".poder");
+catPoder.forEach(poder => {
+  poder.addEventListener("click", function () {
+    buscarPoder(poder.textContent.toUpperCase());
   });
 })
