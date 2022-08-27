9e475d4ea774eadbdcb4abc759d9af289cf672fb
Arturo Renteria <arthur_metal88@hotmail.com>
2022-08-27 18:39:37 -0500

    filtros rol

--- js/filtros.js
+++ js/filtros.js
@@ -1,11 +1,27 @@
 let buscar = (evt) => {
-    evt.preventDefault();
-    let imputBuscar = document.querySelector("#imputBuscar").value.toUpperCase();
+    //evt.preventDefault();
+    let inputBuscar = document.querySelector("#inputBuscar").value.toUpperCase();
     let filtrados = campeones.filter((campeon) => {      
-      return campeon.nombre.toUpperCase().includes(imputBuscar);
+      return campeon.nombre.toUpperCase().includes(inputBuscar);
     });
     console.log(filtrados);
     mostrar(filtrados);
   };
 
-  document.querySelector("#buscarForm").addEventListener("submit", buscar);
+let buscarRol = (rolCampeon) => {
+  let filtro = campeones.filter((campeon) =>{
+    return campeon.rol==rolCampeon;    
+  })
+  mostrar(filtro);
+} 
+
+  //document.querySelector("#buscarForm").addEventListener("submit", buscar);
+  document.querySelector("#buscarForm button").addEventListener("click", buscar);
+  document.querySelector("#inputBuscar").addEventListener("keyup", buscar);
+  
+const catRol =  document.querySelectorAll(".rol");
+catRol.forEach(rol => {
+  rol.addEventListener("click", function () {
+    buscarRol(rol.textContent.toUpperCase());
+  });
+})
